a5a333b349fc6ba44e8f8870694065136006e1c8
MaiKhuu1993 <khuu.tuyetmai@gmail.com>
2024-02-07 00:34:55 -0500

    /movies MVC

---
+++ app/helpers/movies_helper.rb
@@ -0,0 +1,11 @@
+module MoviesHelper
+  def gross(movie)
+    movie.flop? ?
+      "Flop!" :
+      number_to_currency(movie.total_gross, precision: 0)
+  end
+
+  def year_of(movie)
+    movie.released_on.strftime("%Y")
+  end
+end
